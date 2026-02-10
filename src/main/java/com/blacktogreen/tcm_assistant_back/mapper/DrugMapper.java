576530c8817bc72235042713fe7c1310b8428907
Louis Schimchowitsch <schim.louis@gmail.com>
2026-02-10 11:48:13 +0100

    refactor: Use MapStruct for DTO conversions and mappings of drugs

--- src/main/java/com/blacktogreen/tcm_assistant_back/mapper/DrugMapper.java
+++ src/main/java/com/blacktogreen/tcm_assistant_back/mapper/DrugMapper.java
@@ -3,50 +3,22 @@ package com.blacktogreen.tcm_assistant_back.mapper;
 import com.blacktogreen.tcm_assistant_back.dto.*;
 import com.blacktogreen.tcm_assistant_back.model.*;
 import java.util.List;
-import org.springframework.stereotype.Component;
+import org.mapstruct.*;
 
-@Component
-public class DrugMapper {
-  public DrugDto toDto(Drug entity) {
-    return new DrugDto(
-        entity.getId(),
-        entity.getChineseName(),
-        entity.getLatinName(),
-        entity.getFrenchName(),
-        entity.getAlternativeChineseNames(),
-        DrugNatureDto.valueOf(entity.getNature().name()),
-        entity.getFlavors().stream().map(f -> DrugFlavorDto.valueOf(f.name())).toList(),
-        entity.getMovements().stream().map(m -> DrugMovementDto.valueOf(m.name())).toList(),
-        entity.getTropism().stream().map(o -> OrganDto.valueOf(o.name())).toList(),
-        entity.getContraindications(),
-        entity.getEffects(),
-        entity.getDosage(),
-        entity.getNumberOfStars(),
-        entity.getPrimaryCategory().getId(),
-        entity.getAdditionalNotes());
-  }
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface DrugMapper {
 
-  public Drug toEntity(
+  @Mapping(target = "name", source = "chineseName")
+  @Mapping(target = "categoryId", source = "primaryCategory.id")
+  DrugDto toDto(Drug entity);
+
+  @Mapping(target = "chineseName", source = "dto.name")
+  @Mapping(target = "primaryCategory", source = "category")
+  @Mapping(target = "associationsAsOwner", source = "associationsAsOwner")
+  @Mapping(target = "associationsAsAssociated", source = "associationsAsAssociated")
+  Drug toEntity(
       DrugDto dto,
       DrugCategory category,
       List<DrugAssociation> associationsAsOwner,
-      List<DrugAssociation> associationsAsAssociated) {
-    return new Drug(
-        dto.name(),
-        dto.alternativeChineseNames(),
-        dto.latinName(),
-        dto.frenchName(),
-        DrugNature.valueOf(dto.nature().name()),
-        dto.flavors().stream().map(f -> DrugFlavor.valueOf(f.name())).toList(),
-        dto.tropism().stream().map(o -> Organ.valueOf(o.name())).toList(),
-        dto.movements().stream().map(m -> DrugMovement.valueOf(m.name())).toList(),
-        category,
-        dto.contraindications(),
-        dto.effects(),
-        dto.dosage(),
-        dto.numberOfStars(),
-        associationsAsOwner,
-        associationsAsAssociated,
-        dto.additionalNotes());
-  }
+      List<DrugAssociation> associationsAsAssociated);
 }
